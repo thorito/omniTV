e5a381e210039ee01bf415006e27e86edbe4e840
Victor Villar <victorenrique.villar@sivsa.com>
2021-03-30 13:16:04 +0200

    create todo.md

--- app/src/main/java/com/sivsa/omnitv/ui/wizard/WizardActivity.kt
+++ app/src/main/java/com/sivsa/omnitv/ui/wizard/WizardActivity.kt
@@ -1,11 +1,7 @@
 package com.sivsa.omnitv.ui.wizard
 
 import android.Manifest
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import androidx.fragment.app.FragmentActivity
 import androidx.leanback.app.GuidedStepSupportFragment
 import com.karumi.dexter.Dexter
@@ -14,19 +10,12 @@ import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.sivsa.omnitv.R
-import com.sivsa.omnitv.commons.MyBaseApplication
 import com.sivsa.omnitv.tools.TypeToasty
 import com.sivsa.omnitv.tools.toast
 
 class WizardActivity : FragmentActivity() {
 
     private var hasPermissions: Boolean = false
-    private var fromBoot = false
-
-    companion object {
-        private const val RC_INSTALL_APP = 100
-        private const val RC_OVERLAY_PERMISSION = 200
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -90,23 +79,6 @@ class WizardActivity : FragmentActivity() {
         add(Manifest.permission.WAKE_LOCK)
         add(Manifest.permission.SET_ALARM)
         add(Manifest.permission.VIBRATE)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-            add(Manifest.permission.READ_PHONE_STATE)
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            add(Manifest.permission.CHANGE_NETWORK_STATE)
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            add(Manifest.permission.FOREGROUND_SERVICE)
-        }
-
-        if (MyBaseApplication.isTVBox) {
-            add(Manifest.permission.BLUETOOTH)
-            add(Manifest.permission.BLUETOOTH_ADMIN)
-        }
     }
 
     private fun logicOnPermissionChecked(
@@ -114,14 +86,7 @@ class WizardActivity : FragmentActivity() {
         callbackSuccess: () -> Unit
     ) {
         if (report.areAllPermissionsGranted()) {
-
-            hasPermissions = (checkPermissionUnknownAppSources() &&
-                    checkPermissionOverlay())
-
-            if (hasPermissions) {
-                callbackSuccess()
-            }
-
+            callbackSuccess()
         } else {
             hasPermissions = false
             toast(R.string.message_permission_required, type = TypeToasty.ERROR)
@@ -129,62 +94,4 @@ class WizardActivity : FragmentActivity() {
             finish()
         }
     }
-
-    private fun checkPermissionUnknownAppSources(): Boolean {
-
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            if (!packageManager.canRequestPackageInstalls()) {
-                val uri = Uri.parse("package:$packageName")
-                val intent = Intent()
-                intent.action = Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES
-                intent.data = uri
-
-                if (intent.resolveActivity(packageManager) != null) {
-                    startActivityForResult(intent, RC_INSTALL_APP)
-                } else {
-                    toast(R.string.err_permissions_unknown_app_sources, type = TypeToasty.ERROR)
-                }
-
-                false
-            } else {
-                true
-            }
-        } else {
-            true
-        }
-    }
-
-    private fun checkPermissionOverlay(): Boolean {
-        val requestPermission = !MyBaseApplication.isTVBox || Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && requestPermission &&
-            !Settings.canDrawOverlays(this)
-        ) {
-
-            var intent = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-                Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:$packageName"))
-            } else {
-                Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-            }
-
-            if (intent.resolveActivity(packageManager) != null) {
-                startActivityForResult(intent, RC_OVERLAY_PERMISSION)
-            } else {
-
-                intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-                if (intent.resolveActivity(packageManager) != null) {
-                    startActivityForResult(intent, RC_OVERLAY_PERMISSION)
-                } else {
-                    toast(R.string.err_permissions_overlay, type = TypeToasty.ERROR)
-                }
-            }
-
-            false
-        } else {
-            true
-        }
-    }
-
 }
---
+++ todo.md
@@ -0,0 +1,4 @@
+## Tareas a realizar
+1. La aplicación tendra el tema igual que Omni, pero el WizardActivity tendrá el estilo de LeanBack
+2. Widget editable que funcione correctamente en TVBox y móviles.
+3. Checkbox
